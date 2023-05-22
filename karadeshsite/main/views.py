dcff5490772fb98c9b31dca4722b5dced6e272b0
Karadesh <akamalavr@gmail.com>
2023-05-23 02:06:08 +0300

    slugs in my posts added. also test post

--- karadeshsite/main/views.py
+++ karadeshsite/main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotFound, Http404
 from .models import *
 
 #return redirect('adress') for redirect 302 
@@ -14,13 +14,20 @@ menu = [{"title": "Board", "url_name": "board"},
         {"title":"Задонатить", "url_name":"https://www.donationalerts.com/r/karadesh"}]
 
 def index(request):
-    my_posts = MyPosts.objects.all()
     context = {
-        'my_posts': my_posts,
         'menu': menu,
         'title': 'Karadesh site'
     }
     return render(request, "main/index.html", context=context)
+
+def show_post(request, post_slug):
+    post = get_object_or_404(MyPosts, slug=post_slug)
+    context = {
+        'menu': menu,
+        'title': post.title,
+        'post': post
+    }
+    return render(request, "main/show_post.html", context=context)
 
 def feedback(request):
     context = {
@@ -37,11 +44,9 @@ def about(request):
     return render(request, "main/about.html", context=context)
 
 def board(request): #Добавить page_num, чтобы можно было смотреть страницы
-    posts = BoardMessages.objects.all()
     context = {
         'menu': menu,
         'title': 'Board',
-        'posts': posts
     }
     return render(request, "main/board.html", context=context)
 
